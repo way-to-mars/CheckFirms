9694a6d0b4cc50a7cfa7a2bb2ffac14c281d51b7
job-pc <way2mars@hotmail.com>
2023-01-13 14:52:01 +0700

    Jan 13 #job

---
+++ app/src/main/java/com/way2mars/ij/java/checkfirms/AddFirmActivity.java
@@ -0,0 +1,15 @@
+package com.way2mars.ij.java.checkfirms;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddFirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_new_firm);
+    }
+}
