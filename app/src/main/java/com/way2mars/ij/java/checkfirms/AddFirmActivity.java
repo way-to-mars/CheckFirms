bf9a7f53334a697a2245a3f1f14a4ae237fda999
way2mars <way2mars@yandex.ru>
2023-01-16 08:14:49 +0700

    jan 16 #1

--- app/src/main/java/com/way2mars/ij/java/checkfirms/AddFirmActivity.java
+++ app/src/main/java/com/way2mars/ij/java/checkfirms/AddFirmActivity.java
@@ -2,7 +2,6 @@ package com.way2mars.ij.java.checkfirms;
 
 import android.os.Bundle;
 
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -10,7 +9,19 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import org.jetbrains.annotations.NotNull;
 
+
 public class AddFirmActivity extends AppCompatActivity {
+    private Toast localToast=null;
+    private final static String urlPart1 = "https://egrul.itsoft.ru/";
+    private final static String urlPart2 = ".json";
+
+    private void showToast(String string){
+        if(string == null) return;
+        if(string.length()==0) return;
+        if(localToast != null) localToast.cancel();
+        localToast = Toast.makeText(this, string, Toast.LENGTH_LONG);
+        localToast.show();
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,21 +29,31 @@ public class AddFirmActivity extends AppCompatActivity {
         setContentView(R.layout.add_new_firm);
         Button searchButton = findViewById(R.id.anf_button);
         EditText editText = findViewById(R.id.anf_edittext_inn);
-        searchButton.setOnClickListener(view -> {
-                String strInn = editText.getText().toString();
-
-
-            });
+        searchButton.setOnClickListener(view -> loadFirm(editText.getText().toString()));
     }
 
     // the length of INN must be 10 or 12
     private boolean checkInnString(@NotNull String stringInn){
         int strlen = stringInn.length();
-
         if ( strlen == 10 || strlen == 12 ) return true;
-
-        Toast.makeText(this, "ИНН должен состоять из 10 или 12 цифр", Toast.LENGTH_SHORT).show();
+        showToast("ИНН должен состоять из 10 или 12 цифр");
         return false;
     }
 
+    // convert inn to "https://egrul.itsoft.ru/{inn}.json.gz"
+    @NotNull
+    private String convertToUrlString(@NotNull String stringInn){
+        return urlPart1 + stringInn + urlPart2;
+    }
+
+    private void loadFirm(@NotNull String stringInn){
+        if( !checkInnString(stringInn) ) return;
+
+        String urlString = convertToUrlString(stringInn);
+
+        showToast("Строка запроса: " + urlString);
+
+        QueryUtils.fetchFirmData(urlString);
+
+    }
 }
--- app/src/main/java/com/way2mars/ij/java/checkfirms/QueryUtils.java
+++ app/src/main/java/com/way2mars/ij/java/checkfirms/QueryUtils.java
@@ -1,8 +1,21 @@
 package com.way2mars.ij.java.checkfirms;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 
 public final class QueryUtils {
+    private static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
     public static ArrayList<FirmData> createFakeList() {
         ArrayList<FirmData> firmList = new ArrayList<>();
 
@@ -22,5 +35,97 @@ public final class QueryUtils {
         firmList.sort(new FirmDataComparator());
 
         return firmList;
+    }
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL ", e);
+        }
+        return url;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    /**
+     * Make an HTTP request to the given URL and return a String as the response.
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // Closing the input stream could throw an IOException, which is why
+                // the makeHttpRequest(URL url) method signature specifies than an IOException
+                // could be thrown.
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+
+    public static void fetchFirmData(String requestUrl) {
+        // Create URL object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+        }
+
+        //extractFeatureFromJson(jsonResponse);
+        return;
     }
 }
