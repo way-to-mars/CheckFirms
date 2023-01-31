21230e1c622eac8ef1b93bea87d5f29fde584f4a
way2mars <way2mars@yandex.ru>
2023-02-01 00:11:08 +0700

    jan 31 # migrating to RecyclerView

--- app/src/main/java/com/way2mars/ij/java/checkfirms/screen/MainActivity.java
+++ app/src/main/java/com/way2mars/ij/java/checkfirms/screen/MainActivity.java
@@ -3,19 +3,21 @@ package com.way2mars.ij.java.checkfirms.screen;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import android.view.View;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.way2mars.ij.java.checkfirms.data.QueryUtils;
 import com.way2mars.ij.java.checkfirms.R;
+//import com.way2mars.ij.java.checkfirms.databinding.ActivityMainBinding;
 import com.way2mars.ij.java.checkfirms.model.FirmData;
 import com.way2mars.ij.java.checkfirms.data.FirmListAdapter;
+
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -33,41 +35,67 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<FirmData> firms = QueryUtils.createFakeList();
-        ListView listView = findViewById(R.id.firms_list);
+//        ListView listView = findViewById(R.id.firms_list);
+//
+//        final FirmListAdapter adapter = new FirmListAdapter(this, firms);
+//        this.mainArray = firms;
+//        this.mainAdapter = adapter;
+//
+//        listView.setAdapter(adapter);
+//
+//        View viewHeader = getLayoutInflater().inflate(R.layout.firms_list_header, null);
+//        listView.addHeaderView(viewHeader);
+//        listView.addFooterView(viewHeader);
+//
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View view,
+//                                    int position, long id) {
+//                showToast(view.getContext(), "itemClick: position = " + position + ", id = "
+//                        + id, Toast.LENGTH_SHORT);
+//                Log.d(LOG_TAG, "itemClick: position = " + position + ", id = "
+//                        + id + view.getTag());
+//                if( id == -1) addItem();
+//            }
+//        });
+//
+//        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+//            public void onScrollStateChanged(AbsListView view, int scrollState) {
+//                Log.d(LOG_TAG, "scrollState = " + scrollState);
+//            }
+//
+//            public void onScroll(AbsListView view, int firstVisibleItem,
+//                                 int visibleItemCount, int totalItemCount) {
+//                Log.d(LOG_TAG, "scroll: firstVisibleItem = " + firstVisibleItem
+//                        + ", visibleItemCount" + visibleItemCount
+//                        + ", totalItemCount" + totalItemCount);
+//            }
+//        });
 
-        final FirmListAdapter adapter = new FirmListAdapter(this, firms);
-        this.mainArray = firms;
-        this.mainAdapter = adapter;
 
-        listView.setAdapter(adapter);
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+         //   setContentView(R.layout.activity_main);
 
-        View viewHeader = getLayoutInflater().inflate(R.layout.firms_list_header, null);
-        listView.addHeaderView(viewHeader);
-        listView.addFooterView(viewHeader);
+//        setSupportActionBar(binding.toolbar);
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                showToast(view.getContext(), "itemClick: position = " + position + ", id = "
-                        + id, Toast.LENGTH_SHORT);
-                Log.d(LOG_TAG, "itemClick: position = " + position + ", id = "
-                        + id + view.getTag());
-                if( id == -1) addItem();
-            }
-        });
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+            FloatingActionButton fab = findViewById(R.id.fab);
 
-        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-                Log.d(LOG_TAG, "scrollState = " + scrollState);
-            }
+//        binding.fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
-            public void onScroll(AbsListView view, int firstVisibleItem,
-                                 int visibleItemCount, int totalItemCount) {
-                Log.d(LOG_TAG, "scroll: firstVisibleItem = " + firstVisibleItem
-                        + ", visibleItemCount" + visibleItemCount
-                        + ", totalItemCount" + totalItemCount);
-            }
-        });
+            fab.setOnClickListener(view -> {
+                Log.d("main", "fab");
+            });
     }
 
     private void showToast(Context context, CharSequence text, int duration){
@@ -83,6 +111,28 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(MainActivity.this, AddFirmActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
